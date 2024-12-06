1ddf375de8fabd14c56671bbc61285be2b9de032
Seungyeon Han <tiffanyhansy@gmail.com>
2024-12-07 01:49:31 +0900

    ✨ initial set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <h1 className="text-3xl font-bold underline">
+      💡조명들이모여샹들리에가되.
+    </h1>
+  );
 }
