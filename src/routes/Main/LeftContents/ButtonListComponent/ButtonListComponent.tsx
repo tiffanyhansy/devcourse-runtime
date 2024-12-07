1dbc2bc664dd269da63b2b31e1aa2775b3f3d889
Ajihey77 <anjihey77@gmail.com>
2024-12-08 03:09:29 +0900

    🩹 feat: 버튼 스타일 수정

--- src/routes/Main/LeftContents/ButtonListComponent/ButtonListComponent.tsx
+++ src/routes/Main/LeftContents/ButtonListComponent/ButtonListComponent.tsx
@@ -2,11 +2,11 @@ import LinkButton from "./LinkButton";
 
 export default function ButtonListComponent() {
   return (
-    <section className="flex gap-[6px]">
-      <LinkButton />
-      <LinkButton />
-      <LinkButton />
-      <LinkButton />
+    <section className="flex gap-[20px]">
+      <LinkButton icon={"./Edit.svg"} title={"글 작성"} />
+      <LinkButton icon={"./Chat.svg"} title={"게시판"} />
+      <LinkButton icon={"./Group-person.svg"} title={"친구관리"} />
+      <LinkButton icon={"./Clock.svg"} title={"몇시간?"} />
     </section>
   );
 }
