a756bebe0a9c1e07e40a95ff374cedb6527ada1e
Seungyeon Han <tiffanyhansy@gmail.com>
2024-12-08 17:48:11 +0900

    ✨ feat: 마이페이지 route 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // react-router-dom 추가
 import Header from "./routes/LayOut/Header";
 import Main from "./routes/Main/Main";
+import Mypage from "./routes/Mypage/Mypage"; // 마이페이지 컴포넌트 import
 
 export default function App() {
   return (
-    <main className="px-[50px] roboto-medium">
-      <Header />
-      <Main />
-    </main>
+    <Router>
+      <main className="px-[50px] roboto-medium">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/mypage" element={<Mypage />} />
+        </Routes>
+      </main>
+    </Router>
   );
 }
