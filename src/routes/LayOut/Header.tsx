0afa55027aa4c789ac2ddb0468b502afab7494d3
Seungyeon Han <tiffanyhansy@gmail.com>
2024-12-08 13:42:10 +0900

    ♻️ refactor: Header <a> 를 <Link>로 대체

--- src/routes/LayOut/Header.tsx
+++ src/routes/LayOut/Header.tsx
@@ -1,28 +1,32 @@
+import { useNavigate, Link } from "react-router-dom";
+
 export default function Header() {
+  // const navigate = useNavigate();
   return (
     <header className="w-full h-[80px] fixed justify-between flex top-0 left-0 items-center px-[50px] bg-white">
       <article className="flex items-center gap-[30px]">
-        <a href="#">
+        <Link to="#">
           <img
             src={"/public/runtime_logo.svg"}
             alt={"런타임 로고"}
             className="w-[40px] h-[40px] object-cover"
           />
-        </a>
-        <a href="#" className="text-lg">
+        </Link>
+        <Link to="/" className="text-lg">
           홈
-        </a>
-        <a href="#" className="text-lg">
+        </Link>
+        <Link to="#" className="text-lg">
           게시글
-        </a>
+        </Link>
       </article>
       <article className="flex items-center gap-[30px]">
-        <a href="#">
+        <Link to="#">
           <img src={"/public/alarm_icon.svg"} alt={"알람 아이콘"} />
-        </a>
-        <button
+        </Link>
+        <Link
+          to="/mypage"
           className={`w-[40px] h-[40px] rounded-full bg-[url(/public/profile.svg)] bg-center`}
-        ></button>
+        ></Link>
       </article>
     </header>
   );
