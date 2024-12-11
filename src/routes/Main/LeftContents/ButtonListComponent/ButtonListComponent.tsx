52df1f5b525139900cc77d51e6f43ea5b8992855
quokka <ansagi3@naver.com>
2024-12-11 10:59:27 +0900

    에러수정굳

--- src/routes/Main/LeftContents/ButtonListComponent/ButtonListComponent.tsx
+++ src/routes/Main/LeftContents/ButtonListComponent/ButtonListComponent.tsx
@@ -1,24 +1,24 @@
 import { useEditorStore } from "../../../../store/store";
 import { useHowTimeStore } from "../../../../store/store";
 import LinkButton from "./LinkButton";
-// import EditorModal from "../../../../components/editor/EditorModal";
-// import BlogEditor from "../../../../components/editor/BlogEditor";
+import EditorModal from "../../../../components/editor/EditorModal";
+import BlogEditor from "../../../../components/editor/BlogEditor";
 
 export default function ButtonListComponent() {
-  // const { toggleEditor } = useEditorStore();
+  const { toggleEditor } = useEditorStore();
   const { toggleHowTime } = useHowTimeStore();
 
-  // const saveContent = (content: string) => {
-  //   console.log("출간된 내용:", content);
-  //   toggleEditor();
-  // };
+  const saveContent = (content: string) => {
+    console.log("출간된 내용:", content);
+    toggleEditor();
+  };
 
   return (
     <section className="flex gap-[20px]">
       <LinkButton
         icon={"./Edit.svg"}
         title={"글 작성"}
-        // onClick={toggleEditor}
+        onClick={toggleEditor}
       />
       <LinkButton icon={"./Chat.svg"} title={"게시판"} />
       <LinkButton icon={"./Group-person.svg"} title={"친구관리"} />
@@ -27,9 +27,9 @@ export default function ButtonListComponent() {
         title={"몇시간?"}
         onClick={toggleHowTime}
       />
-      {/* <EditorModal>
+      <EditorModal>
         <BlogEditor onSave={saveContent} />
-      </EditorModal> */}
+      </EditorModal>
     </section>
   );
 }
