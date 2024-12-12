6afe09079d360cc8a75ff18d7000e88290a15c9f
wl990@naver.com <wl990@naver.com>
2024-12-12 22:41:40 +0900

    📝 친구목록 API 연결

---
+++ src/api/api.d.ts
@@ -0,0 +1,19 @@
+interface userType {
+  role: string;
+  emailVerified: boolean;
+  banned: boolean;
+  isOnline: boolean;
+  posts: string[];
+  likes: string[];
+  comments: string[];
+  followers: string[];
+  following: string[];
+  notifications: string[];
+  messages: string[];
+  _id: string;
+  fullName: string;
+  email: string;
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+}
