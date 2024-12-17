55cc1c4985885aa4027fd66a44f56a066ff7fdcb
Park Jiwoon <jwninenine@gmail.com>
2024-12-18 00:06:01 +0900

    💄 style: 게시판 스켈레톤 추가

--- src/routes/Community/Community.tsx
+++ src/routes/Community/Community.tsx
@@ -1,10 +1,10 @@
 import { useEffect, useState } from "react";
-import PostPreview from "../../components/Community/PostPreview";
-import { Link } from "react-router";
 import { axiosInstance } from "../../api/axios";
 import { Post_T } from "../../api/api";
 import { usePostStore } from "../../store/postStore";
-import { Tooltip } from "@mui/material";
+import PostPreview from "../../components/Community/PostPreview";
+import ChannelButton from "../../components/Community/ChannelButton";
+import Skeleton from "../../components/Community/Skeleton";
 
 type ChannelId_T = "sw" | "si" | "da" | "ge";
 
@@ -15,22 +15,18 @@ type Props = {
 export default function Community({ channelName = "sw" }: Props) {
   const [posts, setPosts] = useState<Post_T[]>([]);
   const [loading, setLoading] = useState(true);
+  const [activeChannel, setActiveChannel] = useState<ChannelId_T>(channelName);
 
-  const fetchChannels = usePostStore((state) => state.fetchChannels); // fetchChannels 가져오기
-  const channels = usePostStore((state) => state.channels); // 채널 리스트 가져오기
+  const fetchChannels = usePostStore((state) => state.fetchChannels);
+  const channels = usePostStore((state) => state.channels);
 
   useEffect(() => {
     const fetchPosts = async () => {
       setLoading(true);
+      if (channels.length === 0) await fetchChannels();
 
-      // 채널 리스트 비어있을 때 가져오기
-      if (channels.length === 0) {
-        await fetchChannels();
-      }
-
-      // 현재 채널 이름에 해당하는 ID 찾기
       const currentChannel = channels.find(
-        (channel) => channel.name.toLowerCase() === channelName
+        (channel) => channel.name.toLowerCase() === activeChannel
       );
 
       if (currentChannel) {
@@ -38,51 +34,54 @@ export default function Community({ channelName = "sw" }: Props) {
           const response = await axiosInstance.get(
             `/posts/channel/${currentChannel._id}`
           );
-          setPosts(response.data); // 게시글 데이터 저장
+          setPosts(response.data);
         } catch (err) {
           console.error("Error fetching posts:", err);
           setPosts([]);
         }
-      } else {
-        setPosts([]);
+        setLoading(false);
       }
-
-      setLoading(false);
     };
 
     fetchPosts();
-  }, [channelName, channels]);
-
-  console.log(posts);
-
-  if (loading) return <p>Loading...</p>;
+  }, [activeChannel, channels]);
 
   return (
     <>
-      <nav className="mt-[80px] flex py-3 items-center text-lg ">
-        <Tooltip title="소프트웨어 개발" arrow>
-        <div className="mr-4 text-white bg-[#7EACB5] hover:bg-black hover:text-white transition-colors font-bold px-3 py-1 rounded-[10px] w-16 text-center opacity-80 ">
-          <Link to="/community/sw" >SW</Link>
-        </div>
-        </Tooltip>
-        <Tooltip title="시스템/인프라" arrow>
-        <div className="mr-4 text-white bg-[#7EACB5] hover:bg-black hover:text-white transition-colors font-bold px-3 py-1 rounded-[10px] w-16 text-center opacity-80">
-          <Link to="/community/si">SI</Link>
-        </div>
-        </Tooltip>
-        <Tooltip title="데이터/AI 개발" arrow>
-        <div className="mr-4 text-white bg-[#7EACB5] hover:bg-black hover:text-white transition-colors font-bold px-3 py-1 rounded-[10px] w-16 text-center opacity-80">
-          <Link to="/community/da">DA</Link>
-        </div>
-        </Tooltip>
-        <Tooltip title="게임/QA" arrow>
-        <div className="text-white bg-[#7EACB5] hover:bg-black hover:text-white transition-colors font-bold px-3 py-1 rounded-[10px] w-16 text-center opacity-80">
-          <Link to="/community/ge">GE</Link>
-        </div>
-        </Tooltip>
+      <nav className="mt-[80px] flex py-3 items-center text-lg">
+        <ChannelButton
+          title="소프트웨어 개발"
+          link="/community/sw"
+          label="SW"
+          isActive={activeChannel === "sw"}
+          onClick={() => setActiveChannel("sw")}
+        />
+        <ChannelButton
+          title="시스템/인프라"
+          link="/community/si"
+          label="SI"
+          isActive={activeChannel === "si"}
+          onClick={() => setActiveChannel("si")}
+        />
+        <ChannelButton
+          title="데이터/AI 개발"
+          link="/community/da"
+          label="DA"
+          isActive={activeChannel === "da"}
+          onClick={() => setActiveChannel("da")}
+        />
+        <ChannelButton
+          title="게임/QA"
+          link="/community/ge"
+          label="GE"
+          isActive={activeChannel === "ge"}
+          onClick={() => setActiveChannel("ge")}
+        />
       </nav>
       <main className="grid gap-9 grid-cols-[repeat(auto-fill,minmax(300px,1fr))] mt-8 mb-6">
-        {posts && posts.length > 0 ? (
+        {loading ? (
+          Array.from({ length: 10 }).map((_, index) => <Skeleton key={index} />)
+        ) : posts.length > 0 ? (
           posts.map((post) => <PostPreview preview={post} key={post._id} />)
         ) : (
           <div>게시글이 없습니다</div>
