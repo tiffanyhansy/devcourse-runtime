5d5999b1333e69dfc6db60fcb4adc4d3a9a854ee
wl990@naver.com <wl990@naver.com>
2024-12-19 01:22:43 +0900

    💄 feat/post-main : 버튼 아이콘 importing

--- src/routes/Main/LeftContents/ButtonListComponent/ButtonListComponent.tsx
+++ src/routes/Main/LeftContents/ButtonListComponent/ButtonListComponent.tsx
@@ -6,8 +6,7 @@ import BlogEditor from "../../../../components/editor/BlogEditor";
 import { useLoginStore } from "../../../../store/API";
 import LoginDialog from "../../../../components/editor/LoginDialog";
 import Chat from "../../../../asset/images/Chat.svg";
-import Clock from "../../../../asset/images/Clock.svg";
-import Edit from "../../../../asset/images/Edit.svg";
+import GroupPerson from "../../../../asset/images/Group-person.svg";
 
 export default function ButtonListComponent() {
   const { toggleEditor } = useEditorStore();
@@ -18,14 +17,22 @@ export default function ButtonListComponent() {
 
   return (
     <section className="flex gap-[10px]">
-      <LinkButton icon={Edit} title={"글 작성"} onClick={toggleEditor} />
-      <LinkButton icon={"/src/asset/images/Chat.svg"} title={"게시판"} />
       <LinkButton
-        icon={Chat}
+        icon={"/src/asset/images/Edit.svg"}
+        title={"글 작성"}
+        onClick={toggleEditor}
+      />
+      <LinkButton icon={Chat} title={"게시판"} />
+      <LinkButton
+        icon={GroupPerson}
         title={token ? "친구관리" : "유저 검색"}
         onClick={open}
       />
-      <LinkButton icon={Clock} title={"몇시간?"} onClick={toggleHowTime} />
+      <LinkButton
+        icon={"/src/asset/images/Clock.svg"}
+        title={"몇시간?"}
+        onClick={toggleHowTime}
+      />
       <EditorModal>
         <BlogEditor />
       </EditorModal>
