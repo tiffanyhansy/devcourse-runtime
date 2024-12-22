d1d61d7ec07e27a7f68edd178d4536add10650de
quokka <ansagi3@naver.com>
2024-12-22 21:05:03 +0900

    ✅community 컴포넌트에 uuid 추가

--- src/routes/Community/Community.tsx
+++ src/routes/Community/Community.tsx
@@ -6,6 +6,7 @@ import PostPreview from "../../components/Community/PostPreview";
 import ChannelButton from "../../components/Community/ChannelButton";
 import Skeleton from "../../components/Community/Skeleton";
 import { t } from "i18next";
+import { v4 as uuidv4 } from "uuid";
 type ChannelId_T = "sw" | "si" | "da" | "ge";
 
 type Props = {
@@ -102,6 +103,7 @@ export default function Community({ channelName = "sw" }: Props) {
             <PostPreview
               preview={post}
               currentUser={currentUser || t("알 수 없음")}
+              key={uuidv4()}
             />
           ))
         ) : (
