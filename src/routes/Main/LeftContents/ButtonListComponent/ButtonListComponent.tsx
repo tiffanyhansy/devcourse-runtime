57799c19da26ef1234175601e8e008238a17bb2c
wl990@naver.com <wl990@naver.com>
2024-12-23 17:37:03 +0900

    📝 feat/post-chat-develop : 채팅 빨간색 점 로직 useEffect 실행될 때 먼저저 한 번 실행하도록 수정

--- src/routes/Main/LeftContents/ButtonListComponent/ButtonListComponent.tsx
+++ src/routes/Main/LeftContents/ButtonListComponent/ButtonListComponent.tsx
@@ -88,6 +88,7 @@ export default function ButtonListComponent() {
   };
 
   useEffect(() => {
+    getConversations();
     const conversationsInterval = setInterval(() => {
       getConversations();
     }, 3000);
